f1f2700fe4d5d556c7387888c0a02390f3b7d9a2
nohseungrae <ca10205@naver.com>
2019-12-18 11:15:11 +0900

    test

--- server/app.js
+++ server/app.js
@@ -17,16 +17,18 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //====================================================
+app.use("/api/users", userRoute);
+app.use("/api/profile", profileRoute);
+//====================================================
 if (process.env.NODE_ENV === "production") {
   //Set static folder
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
   });
 }
-//====================================================
-app.use("/api/users", userRoute);
-app.use("/api/profile", profileRoute);
 //====================================================
 const port = process.env.PORT || 5000;
 
