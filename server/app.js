137cac84c75578589eecbff4f0f7b385f22f8487
nohseungrae <ca10205@naver.com>
2019-12-18 14:34:25 +0900

    hello

--- server/app.js
+++ server/app.js
@@ -4,13 +4,11 @@ import bodyParser from "body-parser";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
-import cors from "cors";
 //====================================================
 import "./db";
 import userRoute from "./routes/userRoute";
 import profileRoute from "./routes/profileRoute";
 const app = express();
-app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(cookieParser());
@@ -19,14 +17,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //====================================================
 app.use("/api/users", userRoute);
 app.use("/api/profile", profileRoute);
+console.log(__dirname);
+// app.use("/nolla", express.static(path.resolve(__dirname, "../client/build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+// });
 //====================================================
 if (process.env.NODE_ENV === "production") {
   //Set static folder
-  app.use(express.static("client/build"));
+  app.use("/nolla", express.static(path.resolve(__dirname, "../client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "..", "client", "build", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
   });
 }
 //====================================================
